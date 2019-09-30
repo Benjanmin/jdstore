a0e145e83870dd63a52220e114fe7fc65adda986
Benjanmin <893527442@qq.com>
2019-09-29 21:10:22 -0700

    add before_action

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,21 +1,21 @@
 class Admin::ProductsController < ApplicationController
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
   def index
     @products = Product.all
   end
 
-  # def new
-  #   @product = Product.new
-  # end
-  #
-  # def create
-  #   @product = Product.new(product_params)
-  #   if @product.save
-  #     redirect_to admin_product_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(product)
+    else
+      render :new
+    end
+  end
   #
   # def show
   #   @product = Product.find(params[:id])
@@ -28,7 +28,7 @@ class Admin::ProductsController < ApplicationController
   # def update
   #   @product = Product.find(params[:id])
   #   if @product.update(product_params)
-  #     redirect_to admin_product_path,notice :"Update success!"
+  #     redirect_to admin_product_path(product),notice :"Update success!"
   #   else
   #     render :edit
   # end
@@ -36,11 +36,11 @@ class Admin::ProductsController < ApplicationController
   # def destroy
   #   @product = Product.find(params[:id])
   #   @product.destroy
-  #   redirect_to admin_product_path,alert :"Delete success!"
+  #   redirect_to admin_product_path(product),alert :"Delete success!"
   # end
 
-  # private
-  # def product_params
-  #   params.require(:product).permit(:title, :description, :price, :quantity)
-  # end
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
 end
