eb66ff643dfc74e2abf5c7006ccfaa0d6b0709d0
Benjanmin <893527442@qq.com>
2019-09-30 00:10:28 -0700

    crud

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
+  before_action :admin_required
+
   def index
     @products = Product.all
   end
@@ -11,7 +13,7 @@ class Admin::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to admin_product_path(product)
+      redirect_to admin_products_path
     else
       render :new
     end
@@ -21,26 +23,27 @@ class Admin::ProductsController < ApplicationController
   #   @product = Product.find(params[:id])
   # end
   #
-  # def edit
-  #   @product = Product.find(params[:id])
-  # end
-  #
-  # def update
-  #   @product = Product.find(params[:id])
-  #   if @product.update(product_params)
-  #     redirect_to admin_product_path(product),notice :"Update success!"
-  #   else
-  #     render :edit
-  # end
-  #
-  # def destroy
-  #   @product = Product.find(params[:id])
-  #   @product.destroy
-  #   redirect_to admin_product_path(product),alert :"Delete success!"
-  # end
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+       redirect_to admin_products_path, notice: "Update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path, alert: "Delete success!"
+  end
 
   private
   def product_params
-    params.require(:product).permit(:title, :description, :price, :quantity)
+    params.require(:product).permit(:title, :description, :price, :quantity, :image)
   end
 end
