0a81c8987d0839ba685069b3ba88a1c63b162248
Benjanmin <893527442@qq.com>
2019-10-03 21:15:53 -0700

    fix cannot find create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_cart
 
   def admin_required
     if !current_user.admin?
-      redirect_to "/"
+      redirect_to "/", alert: "You are not admin."
     end
   end
-
-  helper_method :current_cart
 
   def current_cart
     @current_cart ||= find_cart
