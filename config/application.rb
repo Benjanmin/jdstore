644bd0054cb9f667bd49cac09fcce6e34f7835cc
Benjanmin <893527442@qq.com>
2019-10-05 00:04:23 -0700

    change

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+config.assets.initialize_on_precompile = false
 Bundler.require(*Rails.groups)
 
 module Jdstore
@@ -13,5 +14,3 @@ module Jdstore
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
-config.assets.initialize_on_precompile = false
